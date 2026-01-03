e8573ba542c4878209563803d4a2e2ea2e573b22
MuraliJulakanti <muralidhar.julakanti@aa.com>
2026-01-03 13:57:57 -0600

    Add PrimeNumberOrNot

---
+++ BasicPrograms/PrimeNumberOrNot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrimeNumberOrNot {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = scanner.nextInt();
+
+        if(isPrime(number)) {
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+    
+    public static boolean isPrime(int num) {
+        for(int i = 2; i <= num / 2; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    }
